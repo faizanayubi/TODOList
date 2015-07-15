55745b7df39f919163912f61192342cfd080a283
faizanayubi <indianayubi@gmail.com>
2015-07-15 13:09:55 +0530

    Database Structure

---
+++ app/src/main/java/org/swiftdeal/todolist/DBOpenHelper.java
@@ -0,0 +1,44 @@
+package org.swiftdeal.todolist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Faizan Ayubi on 15-07-2015.
+ */
+public class DBOpenHelper extends SQLiteOpenHelper{
+
+    //Constants for db name and version
+    private static final String DATABASE_NAME = "todos.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //constants for identifying table and columns
+    public static final String TABLE_TODOS = "todos";
+    public static final String TODO_ID = "_id";
+    public static final String TODO_TEXT = "todoText";
+    public static final String TODO_CREATED = "todoCreated";
+
+    //SQL to create table
+    public static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_TODOS + " ( " +
+                    TODO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    TODO_TEXT + " TEXT, " +
+                    TODO_CREATED + " TEXT default CURRENT_TIMESTAMP" +
+                    ")";
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODOS);
+        onCreate(db);
+    }
+}
--- app/src/main/java/org/swiftdeal/todolist/MainActivity.java
+++ app/src/main/java/org/swiftdeal/todolist/MainActivity.java
@@ -1,5 +1,7 @@
 package org.swiftdeal.todolist;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +14,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBOpenHelper helper = new DBOpenHelper(this);
+        SQLiteDatabase database = helper.getWritableDatabase();
     }
 
     @Override
